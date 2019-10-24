70f88e9fa4e75156f4fb37686f29faab67eaeb8b
Iqbal <iqbalrpambudi@gmail.com>
2019-10-24 16:34:26 +0700

    Initial Commmit

---
+++ models/role.js
@@ -0,0 +1,19 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const role = sequelize.define("role", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    code: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  role.associate = function(models) {
+    role.hasMany(models.user, {
+      foreignKey: "role_id"
+    });
+  };
+  return role;
+};
